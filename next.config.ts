b7b26be1dac6531f1f0ad7840430d96fcdd9ed96
M14koli 9846 <mkoli9846@gmail.com>
2026-01-13 05:06:16 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -23,15 +23,12 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
     ],
-  },
-  devServer: {
-    allowedDevOrigins: ['*.cloudworkstations.dev'],
   },
 };
 
