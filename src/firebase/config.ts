b6597512f39b7f6fcdfc298420b699b323894c5e
M14koli 9846 <mkoli9846@gmail.com>
2026-01-13 05:22:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6312432449-3dd8c",
+  "appId": "1:829333176938:web:baa5494d83dc363f42c013",
+  "apiKey": "AIzaSyBo7d2Nv3ATK2ruyzcj__CPfkNueu-drPQ",
+  "authDomain": "studio-6312432449-3dd8c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "829333176938"
+};
