5698e778a0b054c42bec72c0419e1b0e017e669c
M14koli 9846 <mkoli9846@gmail.com>
2026-02-12 17:49:21 +0000

    allign this properly inside pannel  (_for element span_)

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -5,7 +5,7 @@ export function AppLogo({ className, textClassName }: { className?: string; text
   return (
     <div className={cn('flex items-center gap-2', className)}>
       <Milestone className="h-6 w-6 text-primary" />
-      <span className={cn('font-headline text-xl font-bold', textClassName)}>MY Vehicle Log</span>
+      <span className={cn('font-headline text-lg font-bold', textClassName)}>MY Vehicle Log</span>
     </div>
   );
 }
